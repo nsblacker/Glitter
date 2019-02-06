d70843e6af0cd068da61b9909d12d08f96afba4b
nsblacker <nsblacker@163.com>
2019-02-06 18:20:40 +0800

    hello texture complete

---
+++ Glitter/Sources/HelloTexture/HelloTextureExtend.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 那强 on 2019/2/6.
+//
+
+#include "HelloTextureExtend.h"
---
+++ Glitter/Sources/HelloTexture/HelloTextureExtend.h
@@ -0,0 +1,14 @@
+//
+// Created by 那强 on 2019/2/6.
+//
+
+#ifndef GLITTER_HELLOTEXTUREEXTEND_H
+#define GLITTER_HELLOTEXTUREEXTEND_H
+
+
+class HelloTextureExtend {
+
+};
+
+
+#endif //GLITTER_HELLOTEXTUREEXTEND_H
